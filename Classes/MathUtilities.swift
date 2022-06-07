bd701846ee92dfdcc442a251c14c2309a4985244
Chandrasheela123 <chandrasheela1927@gmail.com>
2022-06-07 10:48:51 +0530

    Added MathUtility class

---
+++ Classes/MathUtilities.swift
@@ -0,0 +1,35 @@
+//
+//  MathUtilities.swift
+//  MyMathLib
+//
+//  Created by cdp on 07/06/22.
+//
+
+import Foundation
+
+public class MathUtility {
+    
+    public let pi = 3.142345
+    
+    public init() {}
+    
+   public func add(a: Int, b: Int) -> Int {
+        
+        return a + b
+    }
+    
+   public func substract(a: Int, b: Int) -> Int {
+        
+        return a - b
+    }
+    
+    public func multiply(a: Int, b: Int) -> Int {
+        
+        return a * b
+    }
+    
+   public func divide(a: Int, b: Int) -> Int {
+        
+        return a / b
+    }
+}
--- Example/MyMathLib/ViewController.swift
+++ Example/MyMathLib/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import MyMathLib
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let mathUtility = MathUtility()
+        
+        var res = mathUtility.add(a: 10, b: 20)
+        res = mathUtility.substract(a: 20, b: 10)
+        res = mathUtility.multiply(a: 10, b: 20)
+       res =  mathUtility.divide(a: 20, b: 2)
+        
+        print(mathUtility.pi)
+            
+            
+        
     }
 
     override func didReceiveMemoryWarning() {
